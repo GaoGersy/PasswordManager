5654952b5685cfcdd83749a4948b36d7a0cdc6e1
unknown <gaogersy@163.com>
2017-06-22 20:08:16 +0800

    完成部分设置界面

--- app/src/main/java/com/gersion/superlock/activity/SettingActivity.java
+++ app/src/main/java/com/gersion/superlock/activity/SettingActivity.java
@@ -3,36 +3,37 @@ package com.gersion.superlock.activity;
 import android.content.Intent;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.gersion.superlock.R;
 import com.gersion.superlock.base.BaseActivity;
 import com.gersion.superlock.utils.MyConstants;
 import com.gersion.superlock.utils.SpfUtils;
 import com.gersion.superlock.utils.ToastUtils;
+import com.gersion.superlock.view.SettingView;
+import com.gersion.superlock.view.TitleView;
 import com.gersion.superlock.view.ToggleButton;
 import com.sdsmdg.tastytoast.TastyToast;
+import com.suke.widget.SwitchButton;
 
 import org.greenrobot.eventbus.EventBus;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class SettingActivity extends BaseActivity {
 
-    @BindView(R.id.tb_auto_login)
-    ToggleButton mTbAutoLogin;
-    @BindView(R.id.tb_is_lock)
-    ToggleButton mTbIsLock;
-    @BindView(R.id.tb_show_pwd)
-    ToggleButton mTbShowPwd;
-    @BindView(R.id.tb_show_update_time)
-    ToggleButton mTbShowUpdateTime;
-    @BindView(R.id.tv_donation)
-    TextView mTvDonation;
-    @BindView(R.id.tv_about)
-    TextView mTvAbout;
+    @BindView(R.id.titleView)
+    TitleView mTitleView;
+    @BindView(R.id.auto_login)
+    SettingView mAutoLogin;
+    @BindView(R.id.open_lock)
+    SettingView mOpenLock;
+    @BindView(R.id.pwd_show)
+    SettingView mPwdShow;
+    @BindView(R.id.update_time_show)
+    SettingView mUpdateTimeShow;
+    @BindView(R.id.donation)
+    SettingView mDonation;
     @BindView(R.id.activity_setting)
     LinearLayout mActivitySetting;
 
@@ -44,19 +45,28 @@ public class SettingActivity extends BaseActivity {
     @Override
     protected void initView() {
         ButterKnife.bind(this);
+        mTitleView.setTitleText("设置");
     }
 
     @Override
     protected void initData() {
-
+        boolean isAutoLogin = SpfUtils.getBoolean(this, MyConstants.IS_AUTO_LOGIN, false);
+        boolean isLock = SpfUtils.getBoolean(this, MyConstants.IS_LOCK, true);
+        boolean isShowPwd = SpfUtils.getBoolean(this, MyConstants.IS_SHOW_PWD, true);
+        boolean isShowUpdateTime = SpfUtils.getBoolean(this, MyConstants.IS_SHOW_UPDATE_TIME, false);
+        mOpenLock.setSwitchStatus(isLock);
+        mPwdShow.setSwitchStatus(isShowPwd);
+        mAutoLogin.setSwitchStatus(isAutoLogin);
+        mUpdateTimeShow.setSwitchStatus(isShowUpdateTime);
+        mAutoLogin.setEnableEffect(isLock);
     }
 
     @Override
     protected void initListener() {
-        mTbAutoLogin.setOnToggleChanged(new ToggleButton.OnToggleChanged() {
+        mAutoLogin.setSwitchChangeListener(new SwitchButton.OnCheckedChangeListener() {
             @Override
-            public void onToggle(boolean on) {
-                if (on) {
+            public void onCheckedChanged(SwitchButton switchButton, boolean b) {
+                if (b) {
                     SpfUtils.putBoolean(SettingActivity.this, MyConstants.IS_AUTO_LOGIN, true);
                     ToastUtils.showTasty(SettingActivity.this, "开启自动登录", TastyToast.SUCCESS);
                 } else {
@@ -65,25 +75,25 @@ public class SettingActivity extends BaseActivity {
                 }
             }
         });
-        mTbIsLock.setOnToggleChanged(new ToggleButton.OnToggleChanged() {
+
+        mOpenLock.setSwitchChangeListener(new SwitchButton.OnCheckedChangeListener() {
             @Override
-            public void onToggle(boolean on) {
-                if (on) {
+            public void onCheckedChanged(SwitchButton switchButton, boolean b) {
+                mAutoLogin.setEnableEffect(b);
+                if (b) {
                     SpfUtils.putBoolean(SettingActivity.this, MyConstants.IS_LOCK, true);
-                    mTbAutoLogin.setEnabled(true);
                     ToastUtils.showTasty(SettingActivity.this, "开启程序锁", TastyToast.SUCCESS);
                 } else {
                     SpfUtils.putBoolean(SettingActivity.this, MyConstants.IS_LOCK, false);
-                    mTbAutoLogin.setEnabled(false);
                     ToastUtils.showTasty(SettingActivity.this, "关闭程序锁", TastyToast.INFO);
                 }
-
             }
         });
-        mTbShowPwd.setOnToggleChanged(new ToggleButton.OnToggleChanged() {
+
+        mPwdShow.setSwitchChangeListener(new SwitchButton.OnCheckedChangeListener() {
             @Override
-            public void onToggle(boolean on) {
-                if (on) {
+            public void onCheckedChanged(SwitchButton switchButton, boolean b) {
+                if (b) {
                     SpfUtils.putBoolean(SettingActivity.this, MyConstants.IS_SHOW_PWD, true);
                     ToastUtils.showTasty(SettingActivity.this, "开启显示密码", TastyToast.SUCCESS);
                 } else {
@@ -93,10 +103,10 @@ public class SettingActivity extends BaseActivity {
                 EventBus.getDefault().postSticky("ChangSwitch");
             }
         });
-        mTbShowUpdateTime.setOnToggleChanged(new ToggleButton.OnToggleChanged() {
+        mUpdateTimeShow.setSwitchChangeListener(new SwitchButton.OnCheckedChangeListener() {
             @Override
-            public void onToggle(boolean on) {
-                if (on) {
+            public void onCheckedChanged(SwitchButton switchButton, boolean b) {
+                if (b) {
                     SpfUtils.putBoolean(SettingActivity.this, MyConstants.IS_SHOW_UPDATE_TIME, true);
                     ToastUtils.showTasty(SettingActivity.this, "开启显示更新时间", TastyToast.SUCCESS);
                 } else {
@@ -105,43 +115,14 @@ public class SettingActivity extends BaseActivity {
                 }
             }
         });
+
+        mDonation.setItemClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toActivity(AboutActivity.class);
+            }
+        });
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        boolean isAutoLogin = SpfUtils.getBoolean(this, MyConstants.IS_AUTO_LOGIN, false);
-        setToggleButton(isAutoLogin, mTbAutoLogin);
-        boolean isLock = SpfUtils.getBoolean(this, MyConstants.IS_LOCK, true);
-        setToggleButton(isLock, mTbIsLock);
-        boolean isShowPwd = SpfUtils.getBoolean(this, MyConstants.IS_SHOW_PWD, true);
-        setToggleButton(isShowPwd, mTbShowPwd);
-        boolean isShowUpdateTime = SpfUtils.getBoolean(this, MyConstants.IS_SHOW_UPDATE_TIME, false);
-        setToggleButton(isShowUpdateTime, mTbShowUpdateTime);
-        if (!isLock) {
-            mTbAutoLogin.setEnabled(false);
-        }
-    }
-
-    private void setToggleButton(boolean isOpen, ToggleButton tb) {
-        if (isOpen) {
-            tb.setToggleOn();
-        } else {
-            tb.setToggleOff();
-        }
-    }
-
-    @OnClick({R.id.tv_donation, R.id.tv_about})
-    public void onClick(View view) {
-        switch (view.getId()) {
-
-            case R.id.tv_donation:
-                startActivity(new Intent(this, AboutActivity.class));
-                break;
-            case R.id.tv_about:
-                startActivity(new Intent(this, AboutActivity.class));
-                break;
-        }
-    }
 
 }
---
+++ app/src/main/java/com/gersion/superlock/view/SettingView.java
@@ -0,0 +1,87 @@
+package com.gersion.superlock.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.gersion.superlock.R;
+import com.suke.widget.SwitchButton;
+
+/**
+ * Created by gersy on 2017/6/22.
+ */
+
+public class SettingView extends RelativeLayout {
+
+    private View mView;
+    private ImageView mGo;
+    private SwitchButton mSwitchButton;
+
+    public SettingView(Context context) {
+        this(context, null);
+    }
+
+    public SettingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mView = LayoutInflater.from(context).inflate(R.layout.view_setting_item, this, true);
+
+        //加载自定义的属性
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SettingView);
+        ImageView icon = (ImageView) mView.findViewById(R.id.iv_icon);
+        TextView tv_title = (TextView) mView.findViewById(R.id.tv_title);
+        TextView tv_divide = (TextView) mView.findViewById(R.id.devider);
+        mSwitchButton = (SwitchButton) mView.findViewById(R.id.switchButton);
+        mGo = (ImageView) mView.findViewById(R.id.iv_go);
+
+        int iconId = a.getResourceId(R.styleable.SettingView_setting_icon, R.drawable.settings);
+        boolean isGoShow = a.getBoolean(R.styleable.SettingView_setting_go_show, true);
+        boolean isSwitchShow = a.getBoolean(R.styleable.SettingView_setting_switch_show, false);
+        boolean isDividerShow = a.getBoolean(R.styleable.SettingView_setting_divider_show, true);
+        String title = a.getString(R.styleable.SettingView_setting_title);
+        tv_title.setText(title);
+        setGoVisible(isGoShow);
+        if (isSwitchShow) {
+            mSwitchButton.setVisibility( VISIBLE);
+            mGo.setVisibility(GONE);
+        }
+        tv_divide.setVisibility(isDividerShow?VISIBLE:GONE);
+        icon.setImageResource(iconId);
+        a.recycle();
+
+    }
+
+    public void setGoVisible(boolean visible) {
+        mGo.setVisibility(visible?VISIBLE:GONE);
+    }
+
+    public void setSwitchVisible(boolean visible) {
+        mSwitchButton.setVisibility(visible?VISIBLE:GONE);
+    }
+
+    public void setSwitchStatus(boolean isChecked){
+        mSwitchButton.setChecked(isChecked);
+    }
+
+    public void setEnableEffect(boolean enable){
+        mSwitchButton.setChecked(enable);
+        mSwitchButton.setEnabled(enable);
+    }
+
+    public void setSwitchChangeListener(SwitchButton.OnCheckedChangeListener listener) {
+        if (mSwitchButton != null) {
+            mSwitchButton.setOnCheckedChangeListener(listener);
+        }
+    }
+
+    public void setItemClickListener(View.OnClickListener listener) {
+        if (mView != null) {
+            mView.setOnClickListener(listener);
+        }
+    }
+
+}
